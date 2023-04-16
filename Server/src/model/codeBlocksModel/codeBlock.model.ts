e0ac0673bb418c8d3e472a74f84c242a263460c8
Matar Jacob <matar.j@outlook.com>
2023-04-16 20:31:39 +0300

    commit deploy check

--- Server/src/model/codeBlocksModel/codeBlock.model.ts
+++ Server/src/model/codeBlocksModel/codeBlock.model.ts
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 import { Schema } from 'mongoose';
 
 export interface ICodeBlock {
-    code: string
+    code: string,
+    name: string
 }
 
 export const codeBlockSchema = new Schema<ICodeBlock>({
-    code: {type: String, required: true}
+    code: {type: String, required: true},
+    name: {type: String, required: true}
 })
 
 export const CodeBlockModel = mongoose.model<ICodeBlock>("codeBlocks", codeBlockSchema)
